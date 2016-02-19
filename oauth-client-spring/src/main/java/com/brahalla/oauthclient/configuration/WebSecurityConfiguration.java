185b73c56e385574738bb764dc76327d149701be
Daniel Cottone <daniel.cottone@egovtn.org>
2016-02-19 15:41:35 -0600

    updating spring client configuration

--- oauth-client-spring/src/main/java/com/brahalla/oauthclient/configuration/WebSecurityConfiguration.java
+++ oauth-client-spring/src/main/java/com/brahalla/oauthclient/configuration/WebSecurityConfiguration.java
@@ -2,15 +2,13 @@ package com.brahalla.oauthclient.configuration;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-@EnableWebMvcSecurity
 @EnableOAuth2Sso
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
@@ -19,7 +17,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
       .authorizeRequests()
         .antMatchers("/css/**").permitAll()
         .antMatchers("/js/**").permitAll()
-        .antMatchers("index.html", "/").permitAll()
         .anyRequest().fullyAuthenticated();
   }
 
